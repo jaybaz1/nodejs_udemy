6952d131d4e77a1b6096654d88f94f3358e0b93a
Baz Jay <jaybaz1@gmail.com>
2017-12-18 14:25:12 +0000

    Completed module requiring your own files

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,12 +1,15 @@
-console.log('Starting App');
+console.log('Starting app.js');
 
 // Internal Modules
 const fs = require('fs');
 const os = require('os');
+const notes = require('./notes.js');
 
+var add = notes.add(5, 10);
+console.log(add);
 
-var user = os.userInfo();
+//var user = os.userInfo();
 
 // fs.appendFile now throws errors use appendFileSync instead
 // Use template string to easily add string and object data
-fs.appendFileSync('greeting.txt', `Hello ${user.username}!`);
+//fs.appendFileSync('greeting.txt', `Hello ${user.username}! You are ${notes.age}.`);
