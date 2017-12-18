3652e1ed63faf11dfd71b1a428924036396263b8
Baz Jay <jaybaz1@gmail.com>
2017-12-18 14:52:21 +0000

    Completed using 3rd party modules

--- notes-node/app.js
+++ notes-node/app.js
@@ -3,10 +3,17 @@ console.log('Starting app.js');
 // Internal Modules
 const fs = require('fs');
 const os = require('os');
+const _ = require('lodash');
 const notes = require('./notes.js');
 
-var add = notes.add(5, 10);
-console.log(add);
+// console.log(_.isString(true));
+// console.log(_.isString('Test'));
+
+var filteredArray = _.uniq(['Baz', 1, 'Baz', 1, 2, 3]);
+console.log(filteredArray);
+
+//var add = notes.add(5, 10);
+//console.log(add);
 
 //var user = os.userInfo();
 
